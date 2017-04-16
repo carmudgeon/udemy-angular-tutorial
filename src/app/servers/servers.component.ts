bdacc6b161fd92afad3d96e623e4ef5392eeb30e
carmudgeon <diego.agudelo2@gmail.com>
2017-04-15 21:06:41 -0500

    added ngFor and server creation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'Test Server';
   serverCreated = false;
+  servers = ['Server 0', 'Server 1'];
 
   constructor() { }
 
@@ -19,6 +20,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created Name is ' + this.serverName;
   }
 
